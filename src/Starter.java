121a97bb8e40735a829a635fc9461c2d341030c8
Oskix <oskar@boxly.pl>
2019-01-12 10:39:15 +0100

    Add func to starter

--- src/Starter.java
+++ src/Starter.java
@@ -1,5 +1,6 @@
 public class Starter {
     public static void main(String[] args) {
         System.out.println("My first git project, jupi!");
+        System.out.println("Pieerwsza zmiana");
     }
 }
