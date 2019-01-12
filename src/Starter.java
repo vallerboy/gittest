8f58567249c73f21542e81e7f1acb54f1c6586ae
agargg <roga.agata@gmail.com>
2019-01-12 11:10:41 +0100

    Add funct1 to starter

--- src/Starter.java
+++ src/Starter.java
@@ -2,5 +2,13 @@ public class Starter {
     public static void main(String[] args) {
         System.out.println("My first git project, jupi!");
         System.out.println("Pieerwsza zmiana");
+
+        welcome1();
+    }
+
+    public static void welcome1() {
+        System.out.println("Mamy konflikt? ");
     }
 }
+
+
