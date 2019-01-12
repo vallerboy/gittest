6591f35c4e6fbd462e29145f0ec01d9511a21fa6
Oskix <oskar@boxly.pl>
2019-01-12 11:07:59 +0100

    Add welcome function to starter

--- src/Starter.java
+++ src/Starter.java
@@ -2,5 +2,11 @@ public class Starter {
     public static void main(String[] args) {
         System.out.println("My first git project, jupi!");
         System.out.println("Pieerwsza zmiana");
+
+        welcome();
+    }
+
+    public static void welcome() {
+        System.out.println("Witaj świecie! ");
     }
 }
